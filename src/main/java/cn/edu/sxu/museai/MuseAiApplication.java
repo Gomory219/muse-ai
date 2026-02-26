ff76399d296144699b078c9978580ba8ec6a16dc
OneFish <15238697391@163.com>
2026-02-26 22:22:18 +0800

    项目后端初始化

---
+++ src/main/java/cn/edu/sxu/museai/MuseAiApplication.java
@@ -0,0 +1,13 @@
+package cn.edu.sxu.museai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuseAiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MuseAiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/edu/sxu/museai/MuseAiApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.edu.sxu.museai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuseAiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
