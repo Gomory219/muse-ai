be5103dd5380ff1a0a48245fe4106a0410faae58
OneFish <15238697391@163.com>
2026-02-28 11:20:31 +0800

    页面初始化

--- muse-ai-front/src/router/index.ts
+++ muse-ai-front/src/router/index.ts
@@ -1,8 +1,26 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import BasicLayout from '@/layouts/BasicLayout.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/',
+      component: BasicLayout,
+      children: [
+        {
+          path: '',
+          name: 'home',
+          component: () => import('@/views/HomeView.vue'),
+        },
+        {
+          path: 'about',
+          name: 'about',
+          component: () => import('@/views/AboutView.vue'),
+        },
+      ],
+    },
+  ],
 })
 
 export default router
