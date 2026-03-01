56a037cddbf171787d61c46a1e3450925a6336cb
OneFish <15238697391@163.com>
2026-03-01 19:48:17 +0800

    前端登录等页面

--- muse-ai-front/src/api/typings.d.ts
+++ muse-ai-front/src/api/typings.d.ts
@@ -1,7 +1,137 @@
 declare namespace API {
+  type BaseResponseBoolean = {
+    code?: number
+    data?: boolean
+    message?: string
+  }
+
+  type BaseResponseListUserVO = {
+    code?: number
+    data?: UserVO[]
+    message?: string
+  }
+
+  type BaseResponseLoginUserVO = {
+    code?: number
+    data?: LoginUserVO
+    message?: string
+  }
+
+  type BaseResponseLong = {
+    code?: number
+    data?: number
+    message?: string
+  }
+
   type BaseResponseString = {
     code?: number
     data?: string
     message?: string
+  }
+
+  type BaseResponseUser = {
+    code?: number
+    data?: User
+    message?: string
+  }
+
+  type BaseResponseUserVO = {
+    code?: number
+    data?: UserVO
+    message?: string
+  }
+
+  type DeleteRequest = {
+    id?: string
+  }
+
+  type getParams = {
+    id: number
+  }
+
+  type getVOParams = {
+    id: number
+  }
+
+  type listParams = {
+    userQueryRequest: UserQueryRequest
+  }
+
+  type LoginUserVO = {
+    id?: number
+    userAccount?: string
+    userName?: string
+    userAvatar?: string
+    userProfile?: string
+    userRole?: 'user' | 'admin'
+    createTime?: string
+    updateTime?: string
+  }
+
+  type saveParams = {
+    userAddRequest: UserAddRequest
+  }
+
+  type User = {
+    id?: number
+    userAccount?: string
+    userPassword?: string
+    userName?: string
+    userAvatar?: string
+    userProfile?: string
+    userRole?: 'user' | 'admin'
+    editTime?: string
+    createTime?: string
+    updateTime?: string
+    isDelete?: number
+  }
+
+  type UserAddRequest = {
+    userName?: string
+    userAccount?: string
+    userAvatar?: string
+    userProfile?: string
+    userRole?: 'user' | 'admin'
+  }
+
+  type UserLoginRequest = {
+    userAccount?: string
+    userPassword?: string
+  }
+
+  type UserQueryRequest = {
+    pageNum?: number
+    pageSize?: number
+    sortField?: string
+    sortOrder?: string
+    id?: number
+    userName?: string
+    userAccount?: string
+    userProfile?: string
+    userRole?: 'user' | 'admin'
+  }
+
+  type UserRegisterRequest = {
+    userAccount?: string
+    userPassword?: string
+    confirmPassword?: string
+  }
+
+  type UserUpdateRequest = {
+    id?: number
+    userName?: string
+    userAvatar?: string
+    userProfile?: string
+    userRole?: 'user' | 'admin'
+  }
+
+  type UserVO = {
+    id?: number
+    userAccount?: string
+    userName?: string
+    userAvatar?: string
+    userProfile?: string
+    userRole?: 'user' | 'admin'
+    createTime?: string
   }
 }
---
+++ muse-ai-front/src/api/userController.ts
@@ -0,0 +1,131 @@
+// @ts-ignore
+/* eslint-disable */
+import request from '@/request'
+
+/** 此处后端没有提供注释 POST /user/delete */
+export async function deleteUsingPost(body: API.DeleteRequest, options?: { [key: string]: any }) {
+  return request<API.BaseResponseBoolean>('/user/delete', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    data: body,
+    ...(options || {}),
+  })
+}
+
+/** 此处后端没有提供注释 GET /user/get */
+export async function get(
+  // 叠加生成的Param类型 (非body参数swagger默认没有生成对象)
+  params: API.getParams,
+  options?: { [key: string]: any }
+) {
+  return request<API.BaseResponseUser>('/user/get', {
+    method: 'GET',
+    params: {
+      ...params,
+    },
+    ...(options || {}),
+  })
+}
+
+/** 此处后端没有提供注释 GET /user/get/login */
+export async function getLoginUser(options?: { [key: string]: any }) {
+  return request<API.BaseResponseLoginUserVO>('/user/get/login', {
+    method: 'GET',
+    ...(options || {}),
+  })
+}
+
+/** 此处后端没有提供注释 GET /user/get/vo */
+export async function getVo(
+  // 叠加生成的Param类型 (非body参数swagger默认没有生成对象)
+  params: API.getVOParams,
+  options?: { [key: string]: any }
+) {
+  return request<API.BaseResponseUserVO>('/user/get/vo', {
+    method: 'GET',
+    params: {
+      ...params,
+    },
+    ...(options || {}),
+  })
+}
+
+/** 此处后端没有提供注释 GET /user/list */
+export async function list(
+  // 叠加生成的Param类型 (非body参数swagger默认没有生成对象)
+  params: API.listParams,
+  options?: { [key: string]: any }
+) {
+  return request<API.BaseResponseListUserVO>('/user/list', {
+    method: 'GET',
+    params: {
+      ...params,
+      userQueryRequest: undefined,
+      ...params['userQueryRequest'],
+    },
+    ...(options || {}),
+  })
+}
+
+/** 此处后端没有提供注释 POST /user/login */
+export async function userLogin(body: API.UserLoginRequest, options?: { [key: string]: any }) {
+  return request<API.BaseResponseLoginUserVO>('/user/login', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    data: body,
+    ...(options || {}),
+  })
+}
+
+/** 此处后端没有提供注释 POST /user/logout */
+export async function userLogout(options?: { [key: string]: any }) {
+  return request<API.BaseResponseBoolean>('/user/logout', {
+    method: 'POST',
+    ...(options || {}),
+  })
+}
+
+/** 此处后端没有提供注释 POST /user/register */
+export async function register(body: API.UserRegisterRequest, options?: { [key: string]: any }) {
+  return request<number>('/user/register', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    data: body,
+    ...(options || {}),
+  })
+}
+
+/** 此处后端没有提供注释 POST /user/save */
+export async function save(
+  // 叠加生成的Param类型 (非body参数swagger默认没有生成对象)
+  params: API.saveParams,
+  options?: { [key: string]: any }
+) {
+  return request<API.BaseResponseLong>('/user/save', {
+    method: 'POST',
+    params: {
+      ...params,
+      userAddRequest: undefined,
+      ...params['userAddRequest'],
+    },
+    ...(options || {}),
+  })
+}
+
+/** 此处后端没有提供注释 POST /user/update */
+export async function update(body: API.UserUpdateRequest, options?: { [key: string]: any }) {
+  return request<API.BaseResponseBoolean>('/user/update', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    data: body,
+    ...(options || {}),
+  })
+}
